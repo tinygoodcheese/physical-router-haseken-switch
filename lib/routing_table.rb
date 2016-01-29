0a03074299059d94b295081c38eb466abbbfd640
Yo Nishiyama <yo.applesya@gmail.com>
2016-01-29 21:34:16 +0900

    dpid is added

--- lib/routing_table.rb
+++ lib/routing_table.rb
@@ -4,25 +4,40 @@ require 'pio'
 class RoutingTable
   include Pio
 
+  MAX_SWITCH = 8
   MAX_NETMASK_LENGTH = 32
 
   def initialize(route)
-    @db = Array.new(MAX_NETMASK_LENGTH + 1) { Hash.new }
+#    @db = Array.new(MAX_NETMASK_LENGTH + 1) { Hash.new }
+    @db = Array.new(MAX_SWITCH+1) { Array.new(MAX_NETMASK_LENGTH+1, Hash.new)}
     route.each { |each| add(each) }
   end
 
   def add(options)
+    # modified by yyynishi, dpip is added
+    dpid = options.fetch(:dpid)
     netmask_length = options.fetch(:netmask_length)
     prefix = IPv4Address.new(options.fetch(:destination)).mask(netmask_length)
-    @db[netmask_length][prefix.to_i] = IPv4Address.new(options.fetch(:next_hop))
+    @db[dpid.hex][netmask_length][prefix.to_i] = IPv4Address.new(options.fetch(:next_hop))
   end
 
-  def lookup(destination_ip_address)
-    MAX_NETMASK_LENGTH.downto(0).each do |each|
-      prefix = destination_ip_address.mask(each)
-      entry = @db[each][prefix.to_i]
-      return entry if entry
+  # modified by uuunishi, dpid is added
+  def lookup(dpid, destination_ip_address)
+     MAX_NETMASK_LENGTH.downto(0).each do |each|
+        prefix = destination_ip_address.mask(each)
+        entry = @db[dpid.hex][each][prefix.to_i]
+        return entry if entry
+      end
+      nil
     end
-    nil
   end
+
+#  def lookup(destination_ip_address)
+#    MAX_NETMASK_LENGTH.downto(0).each do |each|
+#      prefix = destination_ip_address.mask(each)
+#      entry = @db[each][prefix.to_i]
+#      return entry if entry
+#    end
+#    nil
+#  end
 end
